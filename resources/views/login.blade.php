35905498d6ef5fc97e9f1f77812ec0227ae2224d
Michael <gaelle.balois@gmail.com>
2022-10-20 14:44:47 +0200

    ajout formulaire page login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.app-before')
-
+@section('css')
+<link href='/css/login.css' rel='stylesheet'>
+@endsection
 @section('content')
 <section class='login'>
-    <h2>Connexion</h2>
+    <div class='titre'><h2>Connexion</h2></div>
+   
+        <form >
+        <label for="username">Nom de l'équipe :</label>
+        <input type="text" id="login" name="login" placeholder="nom de l'équipe">
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="pass" placeholder="mot de passe">
+        <input type="submit" class="bouton" value="Valider">
+        </form>
+    
 </section>
 
 @endsection('content')
